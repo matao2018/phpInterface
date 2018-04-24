305cb1795d66e4960d45eefc33b52b4822a12c55
matao2018 <1184298784@qq.com>
2018-04-24 14:33:24 +0800

    小程序接口1

--- pdo.php
+++ pdo.php
@@ -1,5 +1,5 @@
 <?php
 	header("content-type:text/html;charset=utf8");
-	$db = new PDO("mysql:host=127.0.0.1;dbname=weixin",'root','root');//初始化一个PDO对象
+	$db = new PDO("mysql:host=mysql;dbname=weixin",'root','root');//初始化一个PDO对象
 	$db->query('set names utf8');
 ?>
